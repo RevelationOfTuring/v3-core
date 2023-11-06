6d3b0fb9be6558194ccf8ee5ac7c1985be750250
MichaelWang <michael_wangguanyi@163.com>
2023-11-06 15:55:34 +0800

    fixed point

--- contracts/libraries/FixedPoint128.sol
+++ contracts/libraries/FixedPoint128.sol
@@ -3,6 +3,8 @@ pragma solidity >=0.4.0;
 
 /// @title FixedPoint128
 /// @notice A library for handling binary fixed point numbers, see https://en.wikipedia.org/wiki/Q_(number_format)
+// 该库的本质是用二进制来表示浮点数
 library FixedPoint128 {
+    // 2**128
     uint256 internal constant Q128 = 0x100000000000000000000000000000000;
 }
--- contracts/libraries/FixedPoint96.sol
+++ contracts/libraries/FixedPoint96.sol
@@ -4,7 +4,10 @@ pragma solidity >=0.4.0;
 /// @title FixedPoint96
 /// @notice A library for handling binary fixed point numbers, see https://en.wikipedia.org/wiki/Q_(number_format)
 /// @dev Used in SqrtPriceMath.sol
+// 注：该库用于SqrtPriceMath.sol
+// 该库的本质是用二进制来表示浮点数
 library FixedPoint96 {
     uint8 internal constant RESOLUTION = 96;
+    // 2**96
     uint256 internal constant Q96 = 0x1000000000000000000000000;
 }
